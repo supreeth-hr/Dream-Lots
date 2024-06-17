b817c14711597f4f5547da6bb176bb3c453107f5
Supreeth Hassan Ravindra <supreeth.hr27@gmail.com>
2024-06-17 10:47:14 -0400

    connect to Database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGOURI)
+    .then(()=>{
+        console.log("Connected to the Database!");
+    })
+    .catch((error)=>{
+        console.log(error);
+    });
 
 const app=express();
 
