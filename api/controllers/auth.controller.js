ada823d5cc063fef3ac728435149800604028d88
Supreeth Hassan Ravindra <supreeth.hr27@gmail.com>
2024-06-17 12:03:19 -0400

    sign up page error handling

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from '../models/user.model.js';
+import {errorHandler} from '../utils/error.js';
 import bcryptjs from 'bcryptjs';
 
-export const signup= async (req,res)=>{
+export const signup= async (req,res,next)=>{
     const {username, email, password}= req.body;
     const hashedPassword=bcryptjs.hashSync(password,10);
     const newUser=new User({username,email,password:hashedPassword});
@@ -9,6 +10,6 @@ export const signup= async (req,res)=>{
         await newUser.save();
         res.status(201).send('User created successfully!');
     }catch(error){
-        res.status(500).send(error.message);
+        next(error);
     }
 } 
