781fa4506fd99bfdee2c0bdcb64c12950327fb5b
Supreeth Hassan Ravindra <supreeth.hr27@gmail.com>
2024-06-17 11:50:18 -0400

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import UserRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGOURI)
@@ -13,9 +14,11 @@ mongoose.connect(process.env.MONGOURI)
     });
 
 const app=express();
+app.use(express.json());
 
 app.listen(3000,()=>{
     console.log("Server is running on PORT 3000");
 });
 
-app.use('/api/user',UserRouter);
+app.use('/api/user', UserRouter);
+app.use('/api/auth', authRouter);
