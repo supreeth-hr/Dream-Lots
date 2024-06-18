082e759674504a2c60cfc47a073c404d99985ee5
Supreeth Hassan Ravindra <supreeth.hr27@gmail.com>
2024-06-18 16:46:14 -0400

    complete sign in page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -21,7 +21,7 @@ export const signin = async (req,res,next)=>{
         const validUser= await User.findOne({email});
         if(!validUser) return next(errorHandler(404,'User not found'));
         const validPassword= await bcryptjs.compareSync(password,validUser.password);
-        if(!validUser) return next(errorHandler(404,'Wrong Credentials! Try Again'));
+        if(!validPassword) return next(errorHandler(404,'Wrong Credentials! Try Again'));
         const token=jwt.sign({id:validUser._id}, process.env.JWT_SECRET);
         const {password: pass,...rest}=validUser._doc;
         res
@@ -31,5 +31,4 @@ export const signin = async (req,res,next)=>{
     }catch(error){
         next(error);
     }
-
 }
