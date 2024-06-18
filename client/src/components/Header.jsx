07aa181a4841fd7153e150e96af0dbdf4623a93e
Supreeth Hassan Ravindra <supreeth.hr27@gmail.com>
2024-06-18 16:24:07 -0400

    create signin api route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,8 +7,8 @@ export default function Header() {
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
             <Link to='/'>
             <h1 className='font-bold text-sm sm:text-3xl flex flex-wrap'>
-                <span className='text-slate-500'>Sup</span>
-                <span>Realtor</span>
+                <span className='text-slate-500 pr-1'>Dream</span>
+                <span>Lots</span>
             </h1>
             </Link>
             <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
