b95df5398e4b204729deaa6bd807097514128b43
Supreeth Hassan Ravindra <supreeth.hr27@gmail.com>
2024-06-20 09:59:54 -0400

    update user api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import UserRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 mongoose.connect(process.env.MONGOURI)
@@ -15,9 +16,10 @@ mongoose.connect(process.env.MONGOURI)
 
 const app=express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000,()=>{
-    console.log("Server is running on PORT 3000");
+    console.log("Server is running on PORT 3000!");
 });
 
 app.use('/api/user', UserRouter);
