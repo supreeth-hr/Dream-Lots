ff0f7bacf7e87c5017a4c3822afb321c90610aa6
Supreeth Hassan Ravindra <supreeth.hr27@gmail.com>
2024-06-20 17:21:03 -0400

    complete listing api route

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import UserRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 dotenv.config();
+import listingRouter from './routes/listing.route.js';
 
 mongoose.connect(process.env.MONGOURI)
     .then(()=>{
@@ -24,6 +25,7 @@ app.listen(3000,()=>{
 
 app.use('/api/user', UserRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
 app.use((err,req,res,next)=>{
     const statusCode= err.statusCode || 500;
