b630deccca07c864282dcea94908c462215db637
Supreeth Hassan Ravindra <supreeth.hr27@gmail.com>
2024-06-22 17:44:56 -0400

    Complete update listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -224,7 +224,9 @@ export default function Profile() {
             </Link>
             <div className='flex flex-col item-center'>
               <button onClick={()=>handleListingDelete(listing._id)}className='text-red-700 uppercase'>delete</button>
+              <Link to={`/update-listing/${listing._id}`}>
               <button className='text-red-700 uppercase'>edit</button>
+              </Link>
             </div>
           </div>
         ))}
