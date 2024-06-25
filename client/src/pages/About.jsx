09d1c15d4e879b17692244d020cade0379b17bd6
Supreeth Hassan Ravindra <supreeth.hr27@gmail.com>
2024-06-25 12:11:33 -0400

    Complete About Page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-
-const About = () => {
+export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Dream Lots</h1>
+      <p className='mb-4 text-slate-700'>fgafdsfgdfgdsgfgsg</p>
+      <p className='mb-4 text-slate-700'>
+      fsdgdsfgfdgdsfg</p>
+      <p className='mb-4 text-slate-700'>wedsxfrtgfdcvghujhbnjkopolkjhbghjuygvcdfrewszxcftyhgbjkiooikjhg</p>
+    </div>
+  );
 }
-
-export default About
