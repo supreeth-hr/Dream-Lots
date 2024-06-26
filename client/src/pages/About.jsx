ba6e1cdec7926d2ab78deb3e04fde6e4d9c60321
Supreeth Hassan Ravindra <supreeth.hr27@gmail.com>
2024-06-26 11:57:09 -0400

    adding example data and about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,11 +1,17 @@
 export default function About() {
   return (
-    <div className='py-20 px-4 max-w-6xl mx-auto'>
-      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Dream Lots</h1>
-      <p className='mb-4 text-slate-700'>fgafdsfgdfgdsgfgsg</p>
-      <p className='mb-4 text-slate-700'>
-      fsdgdsfgfdgdsfg</p>
-      <p className='mb-4 text-slate-700'>wedsxfrtgfdcvghujhbnjkopolkjhbghjuygvcdfrewszxcftyhgbjkiooikjhg</p>
-    </div>
+    <main>
+      <div className='py-10 px-4 max-w-6xl mx-auto'>
+        <h1 className='text-3xl font-bold mb-4 text-slate-800 text-center'>About Dream Lots</h1>
+        <p className='mb-4 text-slate-700 text-justify'>Dream Lots is a leading real estate agency that specializes in helping clients buy, sell, and rent properties in the most desirable neighborhoods. Our team of experienced agents is dedicated to providing exceptional service and making the buying and selling process as smooth as possible.</p>
+        <p className='mb-4 text-slate-700 text-justify'>
+        Our mission is to help our clients achieve their real estate goals by providing expert advice, personalized service, and a deep understanding of the local market. Whether you are looking to buy, sell, or rent a property, we are here to help you every step of the way.</p>
+        <p className='mb-4 text-slate-700 text-justify'>Our team of agents has a wealth of experience and knowledge in the real estate industry, and we are committed to providing the highest level of service to our clients. We believe that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients.</p>
+      </div>
+      <div className='px-4 max-w-6xl mx-auto'>
+        <h1 className='text-3xl font-bold mb-4 text-slate-800 text-center'>About the Creator</h1>
+        <p className='mb-4 text-slate-700 text-justify'>Hello, I am <span className="font-semibold">Supreeth Hassan Ravindra </span>, currently pursuing a Master of Science in Computer Science at Stevens Institute of Technology in Hoboken, NJ. With a strong foundation from my Bachelors in Computer Science at PES College of Engineering in India, where I achieved a CGPA of 9.38/10, I have honed my skills in Algorithms, Data Mining, Machine Learning, and Cloud Computing. During my tenure at Cognizant Technology Solutions India Pvt Ltd, I led projects in Salesforce application development and integration, leveraging technologies like Apex and Lightning Components to enhance business processes and achieve significant efficiency gains. Dream Lots web application is my major project on MERN stack.<br/> Feel free to connect with me via <a href='https://linkedin.com/in/supreethhr' className='text-blue-700 hover:underline' target='_blank'>LinkedIn</a> or check out my projects on <a href='https://github.com/supreeth-hr' className='text-blue-700 hover:underline' target='_blank'> GitHub</a>. </p>
+      </div>
+    </main>
   );
 }
